13a5237bebbf74532c91b71c8a64f88436f23ce8
Maria Vanessa Moreno <MariaVanessaMoreno@outlook.com>
2019-11-04 11:36:16 -0500

    class 4 perp exercises update

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -8,12 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
-// static so all methods inside this class can use
-    static ArrayList<String> cheeses = new ArrayList<>();
+// static so all methods inside this class can use it
+    //replaces arraylist with hashmap to add description of cheese
+    // key: value = cheese: description
+    static HashMap<String, String> cheeses = new HashMap<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
@@ -31,8 +35,27 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
+        //cheeses.add(cheeseName);
+        cheeses.put(cheeseName, cheeseDescription);
         return "redirect:";
+    }
+
+    @RequestMapping(value = "delete", method = RequestMethod.GET)
+    public String displayDeleteCheeseForm(Model model){
+        model.addAttribute("title", "Delete Cheese");
+        model.addAttribute("cheeses", cheeses);
+
+        return "cheese/delete";
+    }
+
+    @RequestMapping(value = "delete", method = RequestMethod.POST)
+    public String DeleteCheeseForm(@RequestParam ArrayList<String> cheeseName) {
+
+        for (String cheese : cheeseName){
+            cheeses.remove(cheese);
+        }
+        return "redirect:";
+
     }
 }
